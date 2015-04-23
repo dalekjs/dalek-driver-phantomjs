2478489a7d7306b9971b019c6f9df261e4a96722
Rodney Rehm <mail+github@rodneyrehm.de>
2015-04-23 18:28:52 +0200

    feature(core): rewriting API to make sense

--- README.md
+++ README.md
@@ -2,37 +2,57 @@
 
 > guineapig for skaro browser configuration
 
-Skaro Browser Binding for [PhantomJS 2](http://phantomjs.org/) providing an API to control the Browser/WebDriver process, as well as provide an easy means to install the necessary binaries etc.
-
-See
+Skaro Browser Binding for [PhantomJS 2](http://phantomjs.org/) providing an API to control the Browser/WebDriver process, buggyfill the browser's WebDriver capabilities, as well as provide an easy means to install the necessary binaries etc.
 
 * [PhantomJS Command Line Interface](http://phantomjs.org/api/command-line.html)
 
 ---
 
-Demo `Dalekfile.json` for configuration
+## API Documentation
 
 ```js
-{
-  // exposing the browser configuration as »phantom«
-  "browser.phantom": {
-    // »phantom« should use the "skaro-browser-phantomjs" package for process control
-    // (required option)
-    "interface": "skaro-browser-phantomjs",
-    // path to binary
-    // default: provided by phantomjs
-    "binary": "/path/to/browser-executable",
-    // make the WebDriver instance listen on interface 127.0.0.1
-    // default: "127.0.0.1"
-    "host": "127.0.0.1",
-    // make the WebDriver instance listen on port 4020
-    // default: 8910
-    "port": 4020,
-    // CLI parameters passed to PhantomJS at startup
-    // default: none
-    "args": [
-      "--local-to-remote-url-access=true"
-    ],
-  }
+var Browser = require('skaro-browser-phantomjs');
+var browser = new Browser({
+  // path to binary
+  // default: provided by phantomjs
+  "binary": "/path/to/browser-executable",
+  // make the WebDriver instance listen on interface 127.0.0.1
+  // default: "127.0.0.1"
+  "host": "127.0.0.1",
+  // make the WebDriver instance listen on a port between 1111 and 2222
+  // default: [2048, 4096]
+  "portRange": [1111, 2222],
+  // CLI parameters passed to PhantomJS at startup
+  // default: (--webdriver=<host:port> --ignore-ssl-errors=true)
+  "args": [
+    // see http://phantomjs.org/api/command-line.html
+    "--local-to-remote-url-access=true"
+  ],
+});
+
+function success(data) {
+  console.log("Started browser, WebDriver available at", data.wd.host, data.wd.port);
 }
+
+// callback invoked when the process could not be started
+function error(err) {
+  console.log("Could not start Browser", err);
+}
+
+// callback invoked when the process crashed
+function failure(err) {
+  console.log("Browser crashed!", err);
+}
+
+// fire up the browser and WebDriver service
+browser.start(success, error, failure);
+
+// gracefully stop the browser
+browser.stop(function() {
+  console.log('stopped!');
+});
+
+// force kill the process (in case stop() doesn't work)
+browser.kill();
 ```
+
--- index.js
+++ index.js
@@ -1,132 +1,169 @@
-module.exports = function DalekBrowserPhantomJSIndex(dalek) {
-  'use strict';  
+'use strict';
 
-  function PhantomJS(options) {
-    this.options = options;
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var portscanner = require('portscanner');
+var expandHomeDir = require('expand-home-dir');
 
-    // (default) config that will be used by the binding
-    this._config = {
-      host: '127.0.0.1',
-      port: [8910, 8910],
-      binary: undefined,
-      args: undefined
-    };
+var phantomPath = require('phantomjs2').path;
 
-    // check & apply configurtion options
-    if (this.options['browser.phantomjs']) {
-      var _options = this.options['browser.phantomjs'];
-      var _port = Array.isArray(_options.port) ? _options.port : [_options.port, _options.port];
-      this._config.port = !!_port[0] ? _port : this._config.port;      
-      this._config.binary = _options.path || this._config.binary;
-      this._config.host = _options.host || this._config.host;
-      this._config.args = Array.isArray(_options.args) ? _options.args : this._config.args;
-    }
-  };
-
-  // Static property to identify the process
-  // TODO: Think about moving static property to some metadata file
-  PhantomJS.id = 'phantomjs';
-
-  PhantomJS.prototype.start = function() {
-    var deferred = dalek.Q.defer();
-
-    // look for an open port
-    this._findSufficientPort(this._config.port[0], this._config.port[1], this._config.host).then(function retrievePort(port) {
-      // options that will be exposed
-      var options = {port: port, host: this._config.host, id: PhantomJS.id};
-
-      // start the process
-      this.process = this._startProcess(options);
-
-      // listen to the process output
-      this.process.stdout.on('data', this._watchStartupProcess(deferred, options));
-      this.process.stderr.on('data', this._processError(deferred, options));
-      this.process.on('close', this._processExit(deferred, options));
-    }.bind(this));
-
-    return deferred.promise;
-  };
-
-  PhantomJS.prototype.stop = function() {
-    this.process.kill('SIGTERM');
-    return this;
-  };
-
-  PhantomJS.prototype._loadBinary = function(binary) {
-    var fs = require('fs');
-    var defaultBinary = require('phantomjs2').path;
-
-    // check if a custom binary has been provided
-    binary = !!binary ? binary : defaultBinary;
-
-    // check if we need to replace the users home directory
-    if (binary !== defaultBinary && process.platform === 'darwin' && binary.trim()[0] === '~') {
-      binary = binary.replace('~', process.env.HOME);
-    }
-
-    // check if the binary exists
-    if (!fs.existsSync(binary)) {
-      return false;
-    }
-
-    return binary;
-  };
-
-  PhantomJS.prototype._findSufficientPort = function(portMin, portMax, address) {
-    var deferred = dalek.Q.defer();
-    var portscanner = require('portscanner');
-
-    portscanner.findAPortNotInUse(portMin, portMax, address, function checkPorts(err, port) {
-      if (err) {
-        deferred.reject('No sufficient port available');
-        return;
-      }
-
-      deferred.resolve(port);
-    });
-
-    return deferred.promise;
-  },
-
-  PhantomJS.prototype._startProcess = function(options) {
-    var phantomjs = this._loadBinary(this._config.binary);
-    var spawn = require('child_process').spawn;
-    var args = ['--webdriver', options.port, '--ignore-ssl-errors=true'];
-    return spawn(phantomjs, dalek._(args).union(this._config.args).unique().value());
-  };
-
-  PhantomJS.prototype._watchStartupProcess = function(deferred, options) {
-    // timeout (ms) / will reject the promise if not started/failes until then
-    var timeout = setTimeout(deferred.reject.bind(deferred, 'Ghost Driver timed out during the startup phase'), 5000);
-
-    // will be called everytime new data comes in from stdout
-    return function watchStdout(data) {
-      var stream = data + '';
-
-      // check if ghostdriver could be launched
-      if (stream.search('GhostDriver - Main - running') !== -1) {
-        deferred.resolve(options);
-        clearTimeout(timeout);
-      } else if (stream.search('Could not start Ghost Driver') !== -1) {
-        deferred.reject('Could not start Ghost Driver');
-        clearTimeout(timeout);
-      }
-    };
-  };
-
-  PhantomJS.prototype._processError = function(deferred) {
-    return function watchStderr(data) {
-      var stream = data + '';
-      deferred.reject('Unexpected process err: ' + stream);
-    };
-  };
-
-  PhantomJS.prototype._processExit = function(deferred) {
-    return function watchExit(data) {
-      var stream = data + '';
-      deferred.reject('Process quit unexpectedly: ' + stream);
-    };
-  };
-
-  return PhantomJS;
+var defaults = {
+  // the host (actually "network interface") to expose WebDriver on
+  host: '127.0.0.1',
+  // all ports are negotiated, but the user might want to use a specific range
+  portRange: [2048, 4096],
+  binary: phantomPath,
+  args: [],
 };
+
+function noop() {}
+
+function Browser(options) {
+  this.options = options || {};
+  Object.keys(defaults).forEach(function(key) {
+    if (this.options[key] === undefined) {
+      this.options[key] = defaults[key];
+    }
+  }, this);
+
+  if (!Array.isArray(this.options.portRange)) {
+    throw new TypeError('option "portRange" must be a range: [min, max]');
+  }
+
+  if (!Array.isArray(this.options.args)) {
+    this.options.args = [this.options.args];
+  }
+
+  if (this.options.binary !== phantomPath) {
+    this.options.binary = expandHomeDir(this.options.binary);
+  }
+}
+
+Browser.prototype.start = function(success, error, failure) {
+  if (this.process) {
+    throw new Error('Process already running');
+  }
+
+  this._findPort(function(port) {
+    this.options.port = port;
+
+    var args = [
+      '--webdriver', this.options.host + ':' + port,
+      '--ignore-ssl-errors=true'
+    ].concat(this.options.args);
+
+    this.process = spawn(this.options.binary, args);
+    this._startListening(success || noop, error || noop, failure || noop);
+  }.bind(this), error);
+};
+
+Browser.prototype.stop = function(callback) {
+  if (!this.process) {
+    return;
+  }
+
+  this.process.removeListener('error', this._handleProcessFailure);
+  this.process.removeListener('close', this._handleProcessFailure);
+
+  this.process.on('close', function(code, signal) {
+    this.process = null;
+    callback && callback();
+  }.bind(this));
+
+  this.process.kill('SIGTERM');
+};
+
+Browser.prototype.kill = function() {
+  if (!this.process) {
+    return;
+  }
+
+  this.process.kill('SIGKILL');
+  this.process = null;
+};
+
+Browser.prototype._findPort = function(success, error) {
+  portscanner.findAPortNotInUse(
+    this.options.portRange[0],
+    this.options.portRange[1],
+    this.options.host,
+    function checkPorts(err, port) {
+      if (err) {
+        return error(new Error('No sufficient port available'));
+      }
+
+      success(port);
+    }
+  );
+};
+
+Browser.prototype._startListening = function(success, error, failure) {
+  this._watchStartupOut = this._watchStartupOut.bind(this, success, error);
+  this._watchStartupErr = this._watchStartupErr.bind(this, success, error);
+  this._handleStartupClose = this._handleStartupClose.bind(this, success, error);
+  this._handleStartupError = this._handleStartupError.bind(this, success, error);
+  this._handleProcessFailure = this._handleProcessFailure.bind(this, failure);
+
+  this.process.on('close', this._handleStartupClose);
+  this.process.on('error', this._handleStartupError);
+  this.process.stdout.on('data', this._watchStartupOut);
+  this.process.stderr.on('data', this._watchStartupErr);
+
+  this._timeout = setTimeout(function() {
+    error(new Error('GhostDriver did not respond within 5s'));
+  }, 5000);
+};
+
+Browser.prototype._stopListening = function() {
+  clearTimeout(this._timeout);
+  this.process.removeListener('close', this._handleStartupClose);
+  this.process.removeListener('error', this._handleStartupError);
+  this.process.stdout.removeListener('data', this._watchStartupOut);
+  this.process.stderr.removeListener('data', this._watchStartupErr);
+  // we're done with the startup phase, now we're observing regular execution time
+  this.process.on('error', this._handleProcessFailure);
+  this.process.on('close', this._handleProcessFailure);
+};
+
+Browser.prototype._watchStartupOut = function(success, error, data) {
+  var _data = String(data);
+
+  if (_data.indexOf('GhostDriver - Main - running') !== -1) {
+    this._stopListening();
+    success({
+      wd: {
+        host: this.options.host,
+        port: this.options.port,
+      }
+    });
+  } else if (_data.indexOf('Could not start Ghost Driver') !== -1) {
+    this._stopListening();
+    this.kill();
+    error(new Error('Could not start Ghost Driver'));
+  }
+};
+
+Browser.prototype._watchStartupErr = function(success, error, data) {
+  this._stopListening();
+  this.kill();
+  error(new Error('Process error: ' + String(data)));
+};
+
+Browser.prototype._handleStartupClose = function(success, error, code) {
+  this._stopListening();
+  this.kill();
+  error(new Error('Process closed with code: ' + code));
+};
+
+Browser.prototype._handleStartupError = function(success, error, err) {
+  this._stopListening();
+  this.kill();
+  error(new Error('Unable to start "' + this.options.binary + '" (' + err + ')'));
+};
+
+Browser.prototype._handleProcessFailure = function(failure, err) {
+  this.kill();
+  failure(new Error('Process quit unexpectedly "' + this.options.binary + '" (' + err + ')'));
+};
+
+module.exports = Browser;
