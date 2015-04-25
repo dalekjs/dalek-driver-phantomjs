9c32f275dcaf0cc0437a59c6b72f26f85f56fbef
Sebastian Golasch <public@asciidisco.com>
2015-04-25 19:25:03 +0200

    chore(lint): Change function arrangement

--- example.js
+++ example.js
@@ -7,6 +7,40 @@ var Driver = require('./index.js');
 var driver = new Driver({
   host: '127.0.1.1'
 });
+
+function stop() {
+  // stop the client
+  wd.quit().then(function() {
+    // then stop the driver
+    driver.stop(function() {
+      // now we're done
+      console.log('bye bye');
+    });
+  });
+}
+
+function haveSomeFunWithWebDriver() {
+  // open a website
+  wd.get('https://google.com')
+    // find all its links
+    .elements('css', 'a')
+    // output what ever elements() returned
+    .then(function(data) {
+      console.log('success', JSON.stringify(data, null, 2));
+    }, function(data) {
+      console.log('error', JSON.stringify(data, null, 2));
+    })
+    // stop WD and the driver
+    .then(stop)
+    // woopsi
+    .catch(function(error) {
+      console.error('failure', error);
+      stop();
+    })
+    // promises…
+    .done();
+}
+
 // start the driver
 driver.start(
   // callback invoked when driver is started
@@ -23,36 +57,3 @@ driver.start(
   // callback invoked when driver crashed after successful start
   console.error.bind(console)
 );
-
-function stop() {
-  // stop the client
-  wd.quit().then(function() {
-    // then stop the driver
-    driver.stop(function() {
-      // now we're done
-      console.log("bye bye");
-    });
-  });
-}
-
-function haveSomeFunWithWebDriver() {
-  // open a website
-  wd.get('https://google.com')
-    // find all its links
-    .elements('css', 'a')
-    // output what ever elements() returned
-    .then(function(data) {
-      console.log("success", JSON.stringify(data, null, 2));
-    }, function(data) {
-      console.log("error", JSON.stringify(data, null, 2));
-    })
-    // stop WD and the driver
-    .then(stop)
-    // woopsi
-    .catch(function(error) {
-      console.error("failure", error);
-      stop();
-    })
-    // promises…
-    .done();
-}
